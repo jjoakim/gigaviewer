5360656ec0040304db5cede6ea57d4eb9de0d905
Fernando Zapata <fzlakers1029@gmail.com>
2020-10-13 17:46:54 -0400

    changed title bar grid list to fix centering issue and prepare it for including the viewer overlay

--- gigaviewer-ui/src/components/grid-view/TitlebarGridList.js
+++ gigaviewer-ui/src/components/grid-view/TitlebarGridList.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 import tileData from './tileData';
@@ -17,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
   },
   gridList: {
-    width: 1200,
+    width: 'auto',
     height: 'auto',
   },
   icon: {
@@ -43,6 +42,7 @@ const useStyles = makeStyles((theme) => ({
  * ];
  */
 
+
 export default function TitlebarGridList() {
   const classes = useStyles();
   const [count, setCount] = useState(0);
@@ -52,40 +52,32 @@ export default function TitlebarGridList() {
     setWidth(window.innerWidth);
   };
   useEffect(() => {
+    document.title = `You clicked ${count} times`;
     resizeWindow();
-    window.addEventListener('resize', resizeWindow);
+    window.addEventListener("resize", resizeWindow);
     console.log(window.innerWidth);
-    return () => window.removeEventListener('resize', resizeWindow);
+    return () => window.removeEventListener("resize", resizeWindow);
   }, []);
 
-  console.log(window.innerWidth);
+  let consoleGrid = (e) => {
+    console.log(e);
+    console.log('GRID');
+  };
+  let consoleIcon = () => {
+    console.log('ICON');
+  };
+
   return (
     <div className={classes.root}>
-      {/* <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button> */}
-      <GridList
-        cellHeight={200}
-        className={classes.gridList}
-        cols={width / 300}
-        spacing={6}
-      >
-        {/* <GridList cellHeight={'200'} className={classes.gridList} cols={6}> */}
-        {/* <GridListTile key="Subheader" cols={4} style={{ height: 'auto' }}>
-          <ListSubheader component="div">LA Lakers Legends</ListSubheader>
-        </GridListTile> */}
+      <GridList cellHeight={200} className={classes.gridList} cols={Math.round(width/300)} spacing={6}>
         {tileData.map((tile) => (
-          <GridListTile key={tile.img}>
-            <img src={tile.img} alt={tile.title} />
+          <GridListTile key={tile.img} onClick={() => {consoleGrid(tile.author)}}>
+            <img src={tile.img} alt={tile.title}/>
             <GridListTileBar
               title={tile.title}
               subtitle={<span>by: {tile.author}</span>}
               actionIcon={
-                <IconButton
-                  aria-label={`info about ${tile.title}`}
-                  className={classes.icon}
-                >
+                <IconButton aria-label={`info about ${tile.title}`} className={classes.icon} onClick={() => {consoleIcon()}}>
                   <InfoIcon />
                 </IconButton>
               }
