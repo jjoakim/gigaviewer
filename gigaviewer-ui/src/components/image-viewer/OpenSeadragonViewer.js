a3ba4a1a6e5b8eaa9c9fcf73abfa96f3f8291258
Fernando Zapata <fzlakers1029@gmail.com>
2020-10-21 02:22:45 -0400

    more prettier changes

--- gigaviewer-ui/src/components/image-viewer/OpenSeadragonViewer.js
+++ gigaviewer-ui/src/components/image-viewer/OpenSeadragonViewer.js
@@ -1,45 +1,46 @@
-import OpenSeaDragon from "openseadragon";
-import React, { useEffect, useState } from "react";
+import OpenSeaDragon from 'openseadragon';
+import React, { useEffect, useState } from 'react';
 
 const OpenSeaDragonViewer = ({ image }) => {
-  const [viewer, setViewer] = useState( null);
+  const [viewer, setViewer] = useState(null);
 
-useEffect(() => {
+  useEffect(() => {
     if (image && viewer) {
       viewer.open(image.source);
     }
   }, [image]);
-const InitOpenseadragon = () => {
+  const InitOpenseadragon = () => {
     viewer && viewer.destroy();
     setViewer(
       OpenSeaDragon({
-        id: "openSeaDragon",
-        prefixUrl: "openseadragon-images/",
+        id: 'openSeaDragon',
+        prefixUrl: 'openseadragon-images/',
         animationTime: 0.5,
         blendTime: 0.1,
         constrainDuringPan: true,
         maxZoomPixelRatio: 2,
         minZoomLevel: 1,
         visibilityRatio: 1,
-        zoomPerScroll: 2
+        zoomPerScroll: 2,
       })
     );
   };
-useEffect(() => {
+  useEffect(() => {
     InitOpenseadragon();
     return () => {
-        viewer && viewer.destroy();
+      viewer && viewer.destroy();
     };
   }, []);
-return (
-  <div 
-  id="openSeaDragon" 
-  style={{
-    height: "800px",
-    width: "1200px"
-  }}
-  >
-  </div>
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div
+      id="openSeaDragon"
+      style={{
+        height: '800px',
+        width: '1200px',
+      }}
+    />
   );
 };
+// eslint-disable-next-line import/prefer-default-export
 export { OpenSeaDragonViewer };
