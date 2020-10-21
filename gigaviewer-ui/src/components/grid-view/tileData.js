1d477b4990d02b5e4894d941b7182318d5cb9749
Fernando Zapata <fzlakers1029@gmail.com>
2020-10-21 01:24:12 -0400

    added thumbnails for new images

--- gigaviewer-ui/src/components/grid-view/tileData.js
+++ gigaviewer-ui/src/components/grid-view/tileData.js
@@ -1,6 +1,8 @@
-import lebronImage from './lebron.jpg';
-import carusoImage from './caruso.jpg';
-import kobeImage from './kobe.jpg';
+import lebronImage from './lebron.jpg'
+import carusoImage from './caruso.jpg'
+import kobeImage from './kobe.jpg'
+import monaImage from './monalisa.jpg'
+import collardImage from './Collard_Green_1.jpg'
 
 const tileData = [
     {
@@ -19,6 +21,16 @@ const tileData = [
       img: kobeImage,
       title: 'Kobe Bryant',
       author: '24',
+    },
+    {
+      img: monaImage,
+      title: 'Mona Lisa',
+      author: 'Leonardo',
+    },
+    {
+      img: collardImage,
+      title: 'Collard Green',
+      author: 'Horstmeyer Lab',
     },
     {
         img: lebronImage,
