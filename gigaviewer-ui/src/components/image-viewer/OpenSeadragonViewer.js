1d6f2810269a94a8c03273b6845a0ac6ff614b08
Fernando Zapata <fzlakers1029@gmail.com>
2020-11-11 21:33:06 -0500

    added scalebar for monalisa

--- gigaviewer-ui/src/components/image-viewer/OpenSeadragonViewer.js
+++ gigaviewer-ui/src/components/image-viewer/OpenSeadragonViewer.js
@@ -9,6 +9,7 @@ import HomeIcon from '@material-ui/icons/Home';
 import FullscreenIcon from '@material-ui/icons/Fullscreen';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import OpenSeadragonImagingHelper from '@openseadragon-imaging/openseadragon-imaginghelper';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,10 +24,12 @@ const OpenSeadragonViewer = ({ images, frame }) => {
   const [viewer, setViewer] = useState(null);
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight);
+  let currentZoom = 0;
+  const [scalebarSize, setScalebarSize] = useState(0);
+  const [scalebarText, setScalebarText] = useState('');
 
   useEffect(() => {
     InitOpenseadragon();
-
     resizeWindow();
     window.addEventListener('resize', resizeWindow);
     console.log('width: ' + window.innerWidth + ' height: ' + window.innerHeight);
@@ -49,16 +52,93 @@ const OpenSeadragonViewer = ({ images, frame }) => {
     if (images && viewer) {
       viewer.open(images[0].frame.source);
     }
+    if(viewer != null) {
+      const imagingHelper = viewer.activateImagingHelper({
+        onImageViewChanged
+      });
+    }
   }, [images]);
+
+  function onImageViewChanged(event) {
+    currentZoom = viewer.viewport.getZoom();
+    console.log(currentZoom);
+    const scaleBarSpecs = getScalebarSizeAndTextForMetric(833.77 / ( 0.28 / currentZoom), 100); // 833.77 = window_height/real_height, 0.28 = defaultZoom
+    setScalebarSize(scaleBarSpecs.size);
+    setScalebarText(scaleBarSpecs.text);
+  }
 
   const resizeWindow = () => {
     setWidth(window.innerWidth);
     setHeight(window.innerHeight);
   };
 
+  function log10(x) {
+    return Math.log(x) / Math.log(10);
+  }
+
+  function getSignificand(x) {
+    return x * Math.pow(10, Math.ceil(-log10(x)));
+  }
+
+  function normalize(value, minSize) {
+    const significand = getSignificand(value);
+    const minSizeSign = getSignificand(minSize);
+    let result = getSignificand(significand / minSizeSign);
+    if (result >= 5) {
+      result /= 5;
+    }
+    if (result >= 4) {
+      result /= 4;
+    }
+    if (result >= 2) {
+      result /= 2;
+    }
+    return result;
+  }
+
+  function getWithUnit(value, unitSuffix) {
+    if (value < 0.000001) {
+      return value * 1000000000 + ' n' + unitSuffix;
+    }
+    if (value < 0.001) {
+      return value * 1000000 + " μ" + unitSuffix;
+    }
+    if (value < 1) {
+      return value * 1000 + " m" + unitSuffix;
+    }
+    if (value >= 1000) {
+      return value / 1000 + " k" + unitSuffix;
+    }
+    return value + " " + unitSuffix;
+  }
+
+  function roundSignificand(x, decimalPlaces) {
+    const exponent = -Math.ceil(-log10(x));
+    const power = decimalPlaces - exponent;
+    const significand = x * Math.pow(10, power);
+    // To avoid rounding problems, always work with integers
+    if (power < 0) {
+      return Math.round(significand) * Math.pow(10, -power);
+    }
+    return Math.round(significand) / Math.pow(10, power);
+  }
+
+  function getScalebarSizeAndTextForMetric(ppm, minSize) {
+    const value = normalize(ppm, minSize);
+    const factor = roundSignificand((value / ppm) * minSize, 3);
+    console.log(value);
+    console.log(factor);
+    const size = value * minSize;
+    const valueWithUnit = getWithUnit(factor, 'm');
+    return {
+      size: size,
+      text: valueWithUnit,
+    };
+  }
+
   const InitOpenseadragon = () => {
     viewer && viewer.destroy();
-    const zoomValue = (height-80)/((11146/7479)*width);
+    const defaultZoom = (height - 80) / ((11146 / 7479) * width);
     setViewer(
       OpenSeaDragon({
         id: 'openSeaDragon',
@@ -66,7 +146,7 @@ const OpenSeadragonViewer = ({ images, frame }) => {
         animationTime: 0.5,
         blendTime: 0.1,
         maxZoomPixelRatio: 2,
-        defaultZoomLevel: zoomValue,
+        defaultZoomLevel: defaultZoom,
         minZoomLevel: 0.2,
         visibilityRatio: 0.5,
         zoomPerScroll: 2,
@@ -114,6 +194,9 @@ const OpenSeadragonViewer = ({ images, frame }) => {
             <IconButton color="primary" aria-label="full screen" disableRipple="true" id="full-page">
               <FullscreenIcon style={{ fontSize: 30 }} />
             </IconButton>
+          </Box>
+          <Box border={1} borderTop={0} bgcolor='rgba(255, 255, 255, 0.5)' width={scalebarSize} position="absolute" bottom="0%" left="44%" zIndex="tooltip" style={{borderRight: '3px solid', borderLeft: '3px solid', borderBottom:'3px solid', textAlign: 'center'}}>
+             {scalebarText}
           </Box>
         </div>
       </Box>
