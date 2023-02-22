afde9f6014936f9d72e05094d205d353537bd178
Joakim <joakim@Gravity.wired.duke.local>
2023-02-21 21:06:01 -0500

    Update

--- test.py
+++ test.py
@@ -106,7 +106,7 @@ def list_dir(path=start_dir, depth=0):
   # if path + "-thumbnail.jpg" in files:
   #   obj["thumbnailImg"] = path + "-thumbnail.jpg"
   if (path + "-thumbnail.jpg") in files:
-    print( [group['thumbnailImg'] for group in obj["groups"].values()] )
+    print( [obj["groups"][name]['thumbnailImg'] for name in obj["groups"]] )
     exit(0)
   
   meta = get_meta()
