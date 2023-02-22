164e3b4f41eed5a3a652c95bfd056aedbdc9fb94
Joakim <joakim@Gravity.wired.duke.local>
2023-02-21 20:12:15 -0500

    Update

--- test.py
+++ test.py
@@ -112,7 +112,7 @@ def list_dir(path=start_dir, depth=0):
     obj["tag"] = meta["tag"]
     tags.append(meta["tag"])
 
-  if not "thumbnailImg" in obj and obj['groups']:
+  if depth > 0 and not "thumbnailImg" in obj and obj['groups']:
     tmp = list(obj["groups"].items())[-1]
     if "thumbnailImg" in tmp[1]:
       obj["thumbnailImg"] = tmp[0] + "/" + tmp[1]["thumbnailImg"]
